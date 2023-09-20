e23dfc02dca9e7f9ac519eb29304e1a047360172
gaurav10997 <avdahika@gmail.com>
2023-09-20 08:56:01 +0530

    complted

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
+
 import React from 'react'
 
 function App() {
   return (
-    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    <div>
+      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+    
   )
 }
 
