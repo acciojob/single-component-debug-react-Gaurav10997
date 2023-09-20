a1030c501acd5271ce7e0d0f5b355456be02d29b
gaurav10997 <avdahika@gmail.com>
2023-09-20 09:02:27 +0530

    complted

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    <div >
+      <p id="main" >Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     </div>
     
   )
